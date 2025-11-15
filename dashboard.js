880ab388c24e14f0bab5ee1afe005e9270c27649
ricardoberetella <ricardoberetella20@gmail.com>
2025-11-15 13:27:21 -0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,20 +1,24 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_KEY;
+const SUPABASE_URL = "https://nfdinjmjofvqmjnfquiy.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mZGluam1qb2Z2cW1qbmZxdWl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxODI1NjEsImV4cCI6MjA3ODc1ODU2MX0.K6dojizNG0oFZaGU9DHZkcbqC8yH--wFDEoaOJGbVYE";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-async function checkUser() {
-  const { data: { user } } = await supabase.auth.getUser();
-  if (!user) window.location.href = "index.html";
+async function entrar() {
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: senha
+    });
+
+    if (error) {
+        alert('Erro: ' + error.message);
+    } else {
+        window.location.href = 'dashboard.html';
+    }
 }
 
-async function logout() {
-  await supabase.auth.signOut();
-  window.location.href = "index.html";
-}
-
-document.getElementById("btnLogout").onclick = logout;
-
-checkUser();
+document.getElementById('btnLogin').onclick = entrar;
