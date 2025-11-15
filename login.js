ec51ade347b5328f639e1cd44d3296138993cacb
ricardoberetella <ricardoberetella20@gmail.com>
2025-11-15 12:03:19 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 // Inicializa o Supabase
 const supabase = supabase.createClient(
   "https://nfdinjmjofvqmjnfquiy.supabase.co", // sua URL Supabase
-  "SUA_ANON_KEY"                              // sua chave pública (anon key)
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mZGluam1qb2Z2cW1qbmZxdWl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxODI1NjEsImV4cCI6MjA3ODc1ODU2MX0.K6dojizNG0oFZaGU9DHZkcbqC8yH--wFDEoaOJGbVYE"                              // sua chave pública (anon key)
 );
 
 // Função de login
