18f4acd42a1fc003eba53896d090195b635067cd
ricardoberetella <ricardoberetella20@gmail.com>
2025-11-16 02:08:10 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,24 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const SUPABASE_URL = "https://nfdinjmjofvqmjnfquiy.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mZGluam1qb2Z2cW1qbmZxdWl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxODI1NjEsImV4cCI6MjA3ODc1ODU2MX0.K6dojizNG0oFZaGU9DHZkcbqC8yH--wFDEoaOJGbVYE";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-document.getElementById('btnLogin').addEventListener('click', async () => {
-    const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
+window.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById('btnLogin');
 
-    const { data, error } = await supabase.auth.signInWithPassword({ email, password: senha });
+    btn.addEventListener('click', async () => {
+        const email = document.getElementById('email').value;
+        const senha = document.getElementById('senha').value;
 
-    if (error) alert("Erro: " + error.message);
-    else window.location.href = 'dashboard.html';
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password: senha
+        });
+
+        if (error) {
+            alert("Erro: " + error.message);
+        } else {
+            window.location.href = 'dashboard.html';
+        }
+    });
 });
