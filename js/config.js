e0a98497c2acd5d9f9dc716a89e34bdd8c77d6bc
ricardoberetella <ricardoberetella20@gmail.com>
2025-11-17 12:29:33 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "https://nfdinjmjofvqmjnfquiy.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mZGluam1qb2Z2cW1qbmZxdWl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxODI1NjEsImV4cCI6MjA3ODc1ODU2MX0.K6dojizNG0oFZaGU9DHZkcbqC8yH--wFDEoaOJGbVYE";
+export const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
+export const SUPABASE_ANON_KEY = "SUA-CHAVE-ANON";
