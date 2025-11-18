7b8209416f5f349b80c33a5aa3131e56cc9aec01
ricardoberetella <ricardoberetella20@gmail.com>
2025-11-17 23:02:46 -0300

    Update recover.js

--- js/recover.js
+++ js/recover.js
@@ -1,4 +1,4 @@
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./js/config.js";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config.js";
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
