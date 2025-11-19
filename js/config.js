3fd5ce67df301752fa4275ff72bb829fe2c663cc
ricardoberetella <ricardoberetella20@gmail.com>
2025-11-19 00:42:22 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 export const SUPABASE_URL = "https://uxtgicfuggpuyjybwawa.supabase.co";
 
-export const SUPABASE_ANON_KEY = "sb_publishable_YCcW-8-tqh_zirjmfvTVEVQ_eZ8gPM5U";
+export const SUPABASE_ANON_KEY =
+  "sb_publishable_YCCw-8-tqh_zirjmfvTVEVQ_eZ8gPM5U";
